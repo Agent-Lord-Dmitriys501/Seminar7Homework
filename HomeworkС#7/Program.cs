ef979769d7080d31492e7a49beb37256293a38d8
Zero <Zero@example.ru>
2022-09-14 20:13:01 +0600

    Решена Задача 50

--- HomeworkС#7/Program.cs
+++ HomeworkС#7/Program.cs
@@ -5,17 +5,61 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+// // создание двухмерного массива (рамдомно)
+// double[,] CreateRandom2dArray(int M, int N)
+// {
+//     double[,] array = new double[M, N];
+//     for (int i = 0; i < M; i++)
+//         for (int j = 0; j < N; j++)
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//     return array;
+// }
+// // вывод двухмерного массива
+// void Show2dArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество столбцов m = ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество строк n = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// double[,] array = CreateRandom2dArray(m, n);
+// Show2dArray(array);
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7->такого числа в массиве нет
+// 1,2 -> 2
+
 // создание двухмерного массива (рамдомно)
-double[,] CreateRandom2dArray(int M, int N)
+int[,] CreateRandom2dArray()
 {
-    double[,] array = new double[M, N];
+    int M = 5;
+    int N = 5;
+    int[,] array = new int[M, N];
     for (int i = 0; i < M; i++)
         for (int j = 0; j < N; j++)
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            array[i, j] = new Random().Next(-100, 101);
     return array;
 }
 // вывод двухмерного массива
-void Show2dArray(double[,] array)
+void Show2dArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -26,12 +70,48 @@ void Show2dArray(double[,] array)
         Console.WriteLine();
     }
 }
+// поиск позиции заданного элемента 
+void PoickElementa(int[,] array, int M, int N)
+{
+    bool element = false;
+    int rezult = 0;
+    // доработать весь метод
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
 
-Console.Write("Введите количество столбцов m = ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == M && j == N)
+            {
+                element = true;
+                rezult = array[i, j];
+            }
+        }
+
+    }
+    Console.WriteLine();
+    if (element == true)
+    {
+        Console.WriteLine(rezult + $" - этот элемент находиться на позиции {M} {N} ");
+    }
+    else
+    {
+        Console.WriteLine($"{M} {N} такой позиции элемента в двухмерном массиве несуществует ");
+    }
+
+}
+// Console.WriteLine(array[i, j] + $" - этот элемент находиться на позиции {M} {N} ");
+// Console.WriteLine($"{M} {N} такой позиции элемента в двухмерном массиве несуществует ");
+
+
+Console.Write("Введите номер позиции элемента в столбце: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите количество строк n = ");
+Console.Write("Введите номер позиции элемента в строке: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-double[,] array = CreateRandom2dArray(m, n);
+
+
+int[,] array = CreateRandom2dArray();
 Show2dArray(array);
+PoickElementa(array, m, n);
